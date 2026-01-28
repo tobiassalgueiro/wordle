73571a2b3b2666e573ce34bced05eb176be0f576
Tobias Salgueiro <tobiassalgueiro24@gmail.com>
2026-01-28 14:50:10 -0300

    se modifico el archivo wordle.py otra vez, se modifico drasticamente el codigo para comenzar de 0

--- wordle.py
+++ wordle.py
@@ -1,10 +1,11 @@
-palabra_secreta = "limon"
 letras_verificadas = []
 cantidad_letras = 5
+palabra_secreta = "holas"
+palabra_ingresada = input("ingresa una palabra ")
+intentos = 0
+
 
 def verificador_palabra(palabra_ingresada, palabra_secreta):
-    letras_verificadas = []
-
     for i in range(cantidad_letras):
         las_palabras_son_iguales = palabra_ingresada[i] == palabra_secreta[i] # True o False
         la_letra_existe_en_la_palabra = palabra_ingresada[i] in palabra_secreta 
@@ -18,19 +19,9 @@ def verificador_palabra(palabra_ingresada, palabra_secreta):
     
     return letras_verificadas
 
-# definir la cantidad de intentos = variable
-intentos = 0
-
 while intentos < 6:
-    print(f"Te quedan {6 - intentos} intentos")
+    print(f"te quedan {6 - intentos} intentos")
     intentos = intentos + 1
-    palabra_ingresada = input("\nIngrese una palabra: ")
-    print(f"\nLa palabra ingresada es: {palabra_ingresada}")
-    respuesta = verificador_palabra(palabra_ingresada, palabra_secreta)
-    print(respuesta)
+    palabra_ingresada = input("Ingrese una palabra")
+    print(f"la palabra ingresada es: {palabra_ingresada}")
 
-    if palabra_ingresada == palabra_secreta:
-        print("\nAcertaste la palabra. GANASTE")
-        break
-    elif intentos == 6:
-        print("\nLlegaste al limite de intentos. PERDEDOR")
